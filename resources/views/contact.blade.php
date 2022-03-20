f8141e4ad0e69d1f7e2986f423c2faa7f0e02c9b
Dev-Andiscipulo <101225367+Dev-Andiscipulo@users.noreply.github.com>
2022-03-20 15:02:58 -0300

    Inicializando Repositorio

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,2 @@
+<h1>seja bem vindo a pagina de contato</h1>
+<a href="\">voltar para a home</a>
---
+++ resources/views/events/create.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.main')
+
+@section ('title', 'Criar Evento')
+
+@section('content')
+
+    <h1>crie um evento</h1>
+
+@endsection
